fcfb9679a90fb67f12cf6690ab69a8bb9557528d
Cristina Zerdan <mariacristinazerdanalzogaraiz@gmail.com>
2025-09-27 14:14:08 -0300

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,7 @@
+const productos = [
+  { id: "silla-moderna", nombre: "Silla Moderna", precio: 12000 },
+  { id: "mesa-rustica", nombre: "Mesa Rústica", precio: 25000 },
+  { id: "sofa-nordico", nombre: "Sofá Nórdico", precio: 48000 }
+];
+
+module.exports = productos;
