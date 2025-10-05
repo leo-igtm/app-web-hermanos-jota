565d90249285ffb79f79d4860dfb2e415a9be2b9
leo-igtm <62738503+leo-igtm@users.noreply.github.com>
2025-10-05 14:07:13 -0300

    feat: se agrego funciones get

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 
 function App() {
+
   return (
+
     <div>
       <h1>Mueblería Jota</h1>
       <p>Bienvenido a nuestra tienda online.</p>
+
     </div>
   );
 }
-
+//use state para manejar productos ,vista actual y carrito
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- productos.js
+++ productos.js
@@ -4,4 +4,38 @@ const productos = [
   { id: "sofa-nordico", nombre: "Sofá Nórdico", precio: 48000 }
 ];
 
+const express = require('express');
+const productosRouter = express.Router();
+const createError = require('http-errors');
+const fs = require('fs/promises');
+const path = require('path');
+const FilePath = path.join(__dirname, '../productos.json');
+
+const leerProductos = async() => {
+  const data = await fs.readFile(FilePath, 'utf-8'); 
+  return JSON.parse(data);
+}
+
+
+// Ruta GET /api/mascotas → devuelve todas las mascotas
+productosRouter.get('/', async(req, res,next) => {
+  leerProductos()
+    .then((productos) => res.json(productos))
+    .catch((err) => next(createError(500, "Error al leeer el archivo")))
+    });
+
+// Ruta para ver una mascota por ID
+productosRouter.get('/:id', async(req, res,next) => {
+  const id = req.params.id;
+  leerProductos()
+    .then((productos) => {
+      const producto = productos.find((p) => p.id === id);
+      if (!producto) 
+        return next(createError(404, "Producto no encontrado"));
+      res.json(producto);
+    })
+    .catch((err) => next(createError(500, "Error al leer el archivo")))
+});  
+
+
 module.exports = productos;
